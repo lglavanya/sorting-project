96cc9ce2552ce55b65547478a7f8f96aab8f6621
lglavanya <lavanyakolar1999@gmail.com>
2023-12-15 17:38:47 +0530

    first file commit

---
+++ Sortt.py
@@ -0,0 +1,11 @@
+def simple_sort(numbers):
+    sorted_numbers = sorted(numbers)
+    return sorted_numbers
+
+
+input_numbers = [5, 2, 8, 1, 7]
+sorted_result = simple_sort(input_numbers)
+
+print(f"Original List: {input_numbers}")
+print(f"Sorted List: {sorted_result}")
+
